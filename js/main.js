5d08e1de2970e135f8a27c2719a5b3c8e9eb806a
david <davidchinccharashvili@gmail.com>
2019-04-30 11:12:15 +0400

    added gallery, work exp

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("working");
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+
+// View an image
+const viewer = new Viewer(document.getElementById('image'), {});
